dd7c83ea93aa2ed49b52fc5d12095aefb76ace90
clairewn <clairewn@umich.edu>
2021-11-25 16:37:32 -0500

    Initial commit

---
+++ calculations.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+"""
+Calculate sum of play counts for top 10 artists in each genre 
+(only for top 10 genres)
+"""
+def sum_play_counts():
+    pass
+
+"""
+Calculate average song lengths for each of the top 100 artists
+"""
+def average_song_length():
+    pass
+
+"""
+Calculate average play counts for each of the top 100 artists
+"""
+def average_play_count():
+    pass
+
+"""
+Main function for this file, calls all function to calculate data and store into database
+"""
+def calculate():
+    sum_play_counts()
+    average_song_length()
+    average_play_count()
