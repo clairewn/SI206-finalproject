afb538f874962e95cef99a27a335383b75e6c86f
clairewn <clairewn@umich.edu>
2021-11-25 20:11:40 -0500

    obtain genres

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import json
+import requests
 
 """
 Connect to Apple API
@@ -12,7 +13,9 @@ def appleAPI():
 Connect to Napster API
 """
 def napsterAPI():
-    pass
+    napsterAPI = 'https://api.napter.com'
+    napster_APIkey = 'OGU2ZWQxNjEtZTI5Yi00MzM1LWE0YTgtNDg5ODZhMjhhZDJm'
+
 
 """
 Connect to Mixcloud API
@@ -23,10 +26,29 @@ def mixcloudAPI():
 """
 Uses Napster API
 Get genres and set up a table of genres with a corresponding integer
+Should be 23 genres
 """
 def obtain_genres():
-    pass
+    response = requests.get('https://https://api.napster.com/v2.0/genres?apikey=OGU2ZWQxNjEtZTI5Yi00MzM1LWE0YTgtNDg5ODZhMjhhZDJm')
+    data = response.text
 
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+'music')
+    cur = conn.cursor()
+
+    cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Genres'")
+    
+    if cur.fetchone()[0]==0: 
+        return
+
+    cur.execute("CREATE TABLE Genres(genre_id INTEGER PRIMARY KEY, genre TEXT")
+    
+    count = 1
+    for item in data["genres"]:
+        cur.execute("INSERT INTO Genres (genre_id, genre) VALUES (?, ?)", (count, item['name']))
+        count += 1
+
+    
 """
 Uses Napster API
 Get top artists and join with genre table
