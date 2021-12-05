4e1c764e46f2cdf5742bc6e5a04d5061c7259a6a
Xiyu Zhong <xiyzhong@0587353484.wireless.umich.net>
2021-12-05 17:13:58 -0500

    histogram 1 done

--- visualizations.py
+++ visualizations.py
@@ -1,5 +1,6 @@
 import setup
 import calculations
+import youtube
 
 import matplotlib.pyplot as plt
 import sqlite3
@@ -8,19 +9,64 @@ import numpy as np
 
 
 """
-Helper function: return a dictionary with Genres as the key and a list of artist subscription count under them
-
+Histogram 1
+Summary of subscriber counts for top 10 artists by genre (only top 10)
 """
 
+def histogram1(cur,conn):
+    command = """
+        SELECT SUM(Subscribers.subscriber_count), Genres.genre
+        FROM Subscribers 
+        JOIN NapsterTopArtists ON Subscribers.artistName = NapsterTopArtists.name
+        JOIN Genres ON Genres.genre_id = NapsterTopArtists.genre
+        GROUP BY Genres.genre;
+        
+    """
+    # command = """
+    #     SELECT ViewCount.view_count, Genres.genre
+    #     FROM Subscribers 
+    #     JOIN TopTracks
+    #     ON Subscribers.artistName = TopTracks.name
+    #     JOIN Genres
+    #     ON Genres.genre_id = TopTracks.
+    #     GROUP BY Genres.genre;
 
-"""
-Histogram
-Summary of play counts for top 10 artists by genre (only top 10)
-"""
-def histogram1(cur, conn):
+    # """  
+    cur.execute(command)
+    list_of_genre_tuples = cur.fetchall()
+    genre_names = []
+    subscribers_sum = []
+    for item in list_of_genre_tuples:
+        genre_names.append(item[1]) #these are the keys
+        subscribers_sum.append(item[0])
 
+    fig, ax = plt.subplots(figsize=(8,7))
+
+    N = len(genre_names)
+    width = 0.35
+    ind = np.arange(N)
+    p1 = ax.bar(ind, subscribers_sum, width, color='blue')
+    ax.set_xticks(ind)
+    ax.set_xticklabels(genre_names, fontsize=10, rotation='vertical')
+
+    ax.autoscale_view()
+    ax.set(xlabel='Genres', ylabel='Sum of Subscriber Counts of Top 10 Artists ', \
+        title='Number of subscribers for top artists in each genre')
+    ax.grid()
+    fig.tight_layout()
+
+    for i in range(len(subscribers_sum)):
+        #if (subscribers_sum[i] > 10000000 or subscribers_sum[i] < 1000):
+            #plt.text(i, subscribers_sum[i], subscribers_sum[i], ha = 'center')
+        plt.text(i, subscribers_sum[i], subscribers_sum[i], fontsize = 5.5, ha = 'center')
+
+    #fig.savefig("name.png")
+    plt.show()
 
     pass
+
+
+
 
 """
 Histogram
@@ -42,6 +88,7 @@ def main():
     # (Ceciel: I commented out these below when I was testing my graphs,
         # since I was trying to slow down the processing time (only using the existing data)
         # please edit if you are using them! Thanks :)
+    
     #setup.setUp()
     #calculations.calculate()
     #histogram1()
@@ -50,7 +97,8 @@ def main():
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+'music.db')
     cur = conn.cursor()
-    histogram1(cur, conn)
+    #histogram1(cur, conn)
+    histogram1(cur,conn)
 
     return 0
 
